d39e897e4ed56e7235e21cc0bb301959373c3eef
KaidDuong <cooleen.cl@gmail.com>
2019-05-05 21:54:08 +0700

    Try to better every day

---
+++ SellWebsite.Ver1.0/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SellWebsite.Ver1._0.Models;
+
+namespace SellWebsite.Ver1._0.Controllers
+{
+    public class HomeController : Controller
+    {
+        QuanLyBanHang1Entities db = new QuanLyBanHang1Entities();
+        // GET: Home
+        public ActionResult Index()
+        {//lan luot tao cac viewbag de lay cac list san pham tu csdl
+
+            //lsit dien thoai moi nhat
+            IEnumerable<SanPham> listNewPhone = db.SanPhams.Where(k => k.MaLoaiSP == 1 && k.Moi == 1 && k.DaXoa == false).ToList();
+            //gan list vao viewbag de truyen qua view
+            ViewBag.ListNewPhone = listNewPhone;
+
+            //list laptop moi nhat
+            var listNewLaptop = db.SanPhams.Where(k => k.MaLoaiSP == 3 && k.Moi == 1 && k.DaXoa == false);
+            ViewBag.ListNewLaptop = listNewLaptop;
+
+            //list Tablet moi nhat
+            var listNewTablet = db.SanPhams.Where(k => k.MaLoaiSP == 2 && k.Moi == 1 && k.DaXoa == false);
+            ViewBag.ListNewTablet = listNewTablet;
+            return View();
+        }
+        public ActionResult MenuPartial()
+        {
+            var listSp = db.SanPhams.ToList<SanPham>();
+            return PartialView(listSp);
+        }
+    }
+}
---
+++ SellWebsite.Ver1.0/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SellWebsite.Ver1._0.Models;
+
+namespace SellWebsite.Ver1._0.Controllers
+{
+    public class ProductController : Controller
+    {
+        SalesManagerEntities db = new SalesManagerEntities();
+        // GET: Product
+        // tao 2 partial view 1 va 2 de show sp len website
+        //use childActiononly de tranh nguoi dung get vao view nay
+        [ChildActionOnly]
+        public ActionResult ProductStylePartial1()
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]
+        public ActionResult ProductStylePartial2()
+        {
+            return PartialView();
+        }
+    }
+}
