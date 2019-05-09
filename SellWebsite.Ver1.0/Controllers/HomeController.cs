9b5d17e531f862aea36f15494fdcafff5b80c7dc
KaidDuong <cooleen.cl@gmail.com>
2019-05-09 13:52:16 +0700

    Built register-site

--- SellWebsite.Ver1.0/Controllers/HomeController.cs
+++ SellWebsite.Ver1.0/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SellWebsite.Ver1._0.Models;
+using CaptchaMvc.HtmlHelpers;
+using CaptchaMvc;
 
 namespace SellWebsite.Ver1._0.Controllers
 {
@@ -28,10 +30,52 @@ namespace SellWebsite.Ver1._0.Controllers
             ViewBag.ListNewTablet = listNewTablet;
             return View();
         }
+        /*
+         *Built  menu for website
+             */
         public ActionResult MenuPartial()
         {
             var listSp = db.SanPhams.ToList<SanPham>();
             return PartialView(listSp);
         }
+
+        /*
+         *Built register page
+         */
+         [HttpGet]
+        public ActionResult Register()
+        {
+            ViewBag.CauHoi = new SelectList(LoadCauHoi());
+            return View();
+        }
+        [HttpPost]
+        public   ActionResult Register(ThanhVien tv)
+         {
+            ViewBag.CauHoi = new SelectList(LoadCauHoi());
+            //Check captcha
+            if(this.IsCaptchaValid("Captcha is not valid")){
+                ViewBag.ThongBao = "Success register";
+                // theem thanh vien vao database
+                db.ThanhViens.Add(tv); // add 1 thanh vien vao dataset
+                db.SaveChanges();// update database
+                return View();
+            }
+            ViewBag.ThongBao = "Wrong captcha !!";
+            return View();
+        }
+        public List<String> LoadCauHoi()
+        {
+            List<String> listCauHoi = new List<string>();
+            listCauHoi.Add("Con vat ban yeu thich la gi");
+            listCauHoi.Add("Bo ban la ai");
+            listCauHoi.Add("Truong ban dang hoc o dau");
+            return listCauHoi;
+
+        }
+        //[HttpPost]
+        //public ActionResult Register(FormCollection formCollection)
+        //{
+        //    return View();
+        //}
     }
 }
